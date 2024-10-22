7373f7c1a3bac26450883792ee33d6c8a47f5a99
danielpatricio0022 <hadukens@hotmail.com>
2024-10-22 11:45:48 -0300

    .

---
+++ src/components/ui/homepage.tsx
@@ -0,0 +1,18 @@
+import { useParams, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+interface Post {
+  id: string;
+  title: string;
+  content: string;
+  imageUrl: string | null;
+}
+
+export function Home() {
+  return(
+    
+    <>
+  <h1>hello</h1>)
+  </>
+  )
+}
